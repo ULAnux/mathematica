8b9e63901a47f66b32976f8ad3d16fd4bf5be841
Patricia Daboin <apdaboin@hotmail.com>
2016-11-06 20:18:40 -0400

    Definición de la diferencia simétrica de dos conjuntos

---
+++ patricia_daboin_21169993_3.pl
@@ -0,0 +1,1 @@
+pr(21169993-3, ['Describa la diferencia simétrica de dos conjuntos A y B'], ['Es el conjunto de los elementos que están en A y en B, pero no en ambos simultáneamente'], _, ['Patricia Daboin']).

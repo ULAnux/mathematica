c38edd0d4056cade2dbf9a1d04c959f9677cfb5d
Gustavo Mejia <bfmvtm@gmail.com>
2016-11-18 17:17:54 -0500

    Update Arbol_Fibonacci.pl

--- src-sin-ubicar/Arbol_Fibonacci.pl
+++ src-sin-ubicar/Arbol_Fibonacci.pl
@@ -1,5 +1,7 @@
 %Gustavo Mejia
 %CI 25302093
-%Altura de un arbol de Fibonacci
+%Demostrar por inducción que la altura de un arbol de Fibonacci de orden K es K
 
-pr(0028-1, [demostrar, induccion, altura, arbol, 'Fibonacci', orden, k, es, k], ['T = Arbol Fibonacci -- Caso base: F(T0) = 0 ; F(T1) = 1 -- Paso inductivo: F(Tk) = k ; F(T(k+1)) = k + 1 -- F(T(k+1)) = 1 + max(F(Tk), F(T(k-1))) -- F(T(k+1)) = 1 + F(Tk) -- F(T(k+1)) = 1 + k = k + 1'], 20/20, ['Gustavo Mejia']).
+pr(0028-1, ['demostrar, induccion, altura, arbol, Fibonacci, orden, k, es, k'], 
+['T = Arbol Fibonacci -- Caso base: F(T0) = 0 ; F(T1) = 1 -- Paso inductivo: F(Tk) = k ; F(T(k+1)) = k + 1 -- F(T(k+1)) = 1 + max(F(Tk), F(T(k-1))) -- F(T(k+1)) = 1 + F(Tk) -- F(T(k+1)) = 1 + k = k + 1'], 
+20/20, ['Gustavo Mejia']).

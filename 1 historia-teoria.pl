6fe397a3e0d456243e5a3ad2db9f6cd0a7873c7f
aespaben <a_espaben@outlook.com>
2016-12-06 10:56:36 -0400

    Create 1 historia-teoria.pl

---
+++ 1 historia-teoria.pl
@@ -0,0 +1,26 @@
+%Trasfondo histórico del sistema numérico vigesimal de los mayas, teoría
+% y ejercicios. Por Angel España - CI: 24.937.226
+
+pr(24937226-01, ['¿Para qué idearon los mayas un sistema de numeración vigesimal?'], ['Para medir el tiempo.'], 20/20 ,['Angel España']).
+
+pr(24937226-02, ['Describa cuántas modalidades tenían los mayas para representar gráficamente los números del 0 al 19.'], ['Tenían tres modalidades: un sistema numérico de puntos y rayas; una numeración cefalomorfa, con variantes de cabezas; y una numeración antropomorfa, mediante figuras completas.'], 20/20, ['Angel España']).
+
+pr(24937226-03, ['¿Qué es un año bisiesto?'], ['Es un año con 366 días, en lugar de 365, en el que febrero tiene 29 días en lugar de 28. Se repite cada cuatro años, excepto cuando el año acaba en dos ceros.'], 20/20, ['Angel España']).
+
+pr(24937226-04, ['¿Cómo se escriben las cantidades en el sistema de numeración maya?'], ['Son agrupadas de 20 en 20; por esa razón en cada nivel puede ponerse cualquier número del 0 al 19.'], 20/20, ['Angel España']).
+
+pr(24937226-05, ['¿Cómo se posicionan estas cantidades'], ['Se posicionan de acuerdo a niveles, donde cada nivel puede contener máximo 20 unidades y cada nivel es una potencia de 20, siendo 20^0 el primer nivel'], 20/20, ['Angel España']).
+
+pr(24937226-06, ['¿Cuáles son los símbolos básicos que se utilizan para representar los números?'], ['El punto, la raya y el caracol, los cuales representan el 1, el 5 y el 0, respectivamente'], 20/20, ['Angel España']).
+
+pr(24937226-07, ['Exprese en sistema decimal la siguiente cantidad expresada en sistema vigesimal: ', [2, 14], ' donde el número más a la derecha representa el primer nivel.', [54], 20/20, ['Angel España']]).
+
+pr(24937226-08, ['Exprese en sistema decimal la siguiente cantidad expresada en sistema vigesimal: ', [2, 4, 19]], [899], 20/20, ['Angel España']).
+
+pr(24937226-09, ['Exprese en sistema vigesimal la siguiente cantidad expresada en sistema decimal: ', [67]], [3, 7], 20/20, ['Angel España']).
+
+pr(24937226-10, ['Exprese en sistema vigesimal la siguiene cantidad expresada en sistema decimal: ', [120]], [6, 0], 20/20, ['Angel España']).
+
+pr(24937226-11, ['Exprese en sistema vigesimal la siguiente cantidad expresada en sistema decimal: ', [863]], [2, 3, 3], 20/20, ['Angel España']).
+
+pr(24937226-12, ['Exprese en sistema decimal la siguiente cantidad expresada en sistema vigesimal: ', [14, 5, 19]], [5719], 20/20, ['Angel España']).

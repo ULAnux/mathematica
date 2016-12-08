513c60bc58d9995301edb1f36e3491883c057ca4
REI94 <josemrd1994@gmail.com>
2016-12-07 22:28:22 -0400

    Add files via upload

---
+++ Maquinas-de-Turing/Bombe/En-C_plus_plus/Release/tscrypt3.bat
@@ -0,0 +1,5 @@
+@rem enigma_cxx XEOXOFXVBAALG AUFKLAVRUNGRA x-xxxxxxxxxxx >tsm3r1.txt
+@rem enigma_cxx XFFZCOBHFBQFYVX BISEINSVIERXVIE xxxx--xxxx-xxxx >tsm3r2.txt
+@rem enigma_cxx XEOXOFXVBAALGZHJKXKNFG...EJTEBYVJVEBSSX AUFKLAVRUNGRAUMADRIAXE...VIERXZWOFUENFB x-x-xxxx--xxx----------------t---x----x >tsm3r3.txt
+enigma_cxx FZCOBHFBQFYVXJAPHX SEINSVIERXVIERNULL xx..xxxx.xxxx...xx >tsm3r4.txt
+
